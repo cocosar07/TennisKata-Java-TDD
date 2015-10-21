251aa763f939e78e389e8275a86d18e230d10b69
chayc <cocosar07370@gmail.com>
2015-10-21 16:30:26 +0200

    7th iteration: Green

--- src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
+++ src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
@@ -1,16 +1,21 @@
 package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
 public class TennisGame
 {
+	public static final String LOVE_FIFTEEN = "Love-Fifteen";
 	public static final String FIFTEEN_LOVE = "Fifteen-Love";
 	public static final String LOVE_ALL = "Love-all";
 	public static final int FIFTEEN_SCORE = 1;
 	
 	private int serverScore;
+	private int receiverScore;
 
 	public String getScoreAsString()
 	{
 		if(this.serverScore == FIFTEEN_SCORE)
 			return FIFTEEN_LOVE;
+		
+		if(this.receiverScore == FIFTEEN_SCORE)
+			return LOVE_FIFTEEN;
 		
 		return LOVE_ALL;
 	}
@@ -18,5 +23,10 @@ public class TennisGame
 	public void serverWonThePoint()
 	{
 		this.serverScore += 1;
+	}
+
+	public void receiverWonThePoint()
+	{
+		this.receiverScore += 1;
 	}
 }
