a170a31acc97752e840c267858c0131938c1b5e3
chayc <cocosar07370@gmail.com>
2015-10-21 16:09:59 +0200

    4th iteration: Green

--- src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
+++ src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
@@ -1,10 +1,18 @@
 package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
 public class TennisGame
 {
+	private int serverScore;
 
 	public String getScoreAsString()
 	{
+		if(this.serverScore == 1)
+			return "Fifteen-Love";
+		
 		return "Love-all";
 	}
 
+	public void increaseServerScore()
+	{
+		this.serverScore += 1;
+	}
 }
